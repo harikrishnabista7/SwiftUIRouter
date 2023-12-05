651b5f5f8113886bbbc35537ebab714a63b4846a
Hari Krishna Bista <harikrishna.bista@ebpearls.com>
2023-12-05 21:42:33 +0545

    package support for ios mac watch os

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftUIRouter",
-    platforms: [.iOS(.v16)],
+    platforms: [.iOS(.v16), .macOS(.v13), .watchOS(.v9)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
